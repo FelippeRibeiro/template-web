2f538d44d105aee3b5d648ba99b63a9d092ab867
Felippe Ribeiro <felipper43@gmail.com>
2025-08-14 10:53:51 -0300

    FIX: remove spaces in classname

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -18,11 +18,11 @@ export default function NotFound() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center dark px-4">
       <div className="text-center">
-        <h1 className="text-9xl font-extrabold ">404</h1>
-        <h2 className="mt-2 text-3xl font-bold ">Página não encontrada</h2>
+        <h1 className="text-9xl font-extrabold">404</h1>
+        <h2 className="mt-2 text-3xl font-bold">Página não encontrada</h2>
         <p className="mt-2 ">Oops! A pagina que você está procurando não existe ou foi movida.</p>
         <p className="mt-2 ">Você será redirecionado em {time} segundos.</p>
-        <Link href="/" className="mt-6 inline-block rounded-lg bg-blue-600 px-6 py-3 text-white font-semibold shadow hover:bg-blue-700 transition-all ">
+        <Link href="/" className="mt-6 inline-block rounded-lg bg-blue-600 px-6 py-3 text-white font-semibold shadow hover:bg-blue-700 transition-all">
           Retornar para o Início
         </Link>
       </div>
